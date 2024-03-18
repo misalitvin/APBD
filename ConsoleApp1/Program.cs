2627c5b596be9f05910b231ef6e404f5472c9437
misalitvin <misalitvin100@gmail.com>
2024-03-18 21:57:26 +0100

    feature-new

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@ static int AveArr(int[] array)
     int sum = 0;
     for(int j = 0;j<array.Length;j++)
     {
-        sum += array[j];
+        sum += array[j]; 
     }
 
     return sum/array.Length;
